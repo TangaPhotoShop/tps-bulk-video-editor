8830d33bb712c87f89b412cd51cbdf70ce7b483f
TangaPhotoShop <steve@tangaloomaphotoshop.com>
2026-09-15 13:48:23 +1000

    Show Version 1.3.10 in the app

--- desktop/app.py
+++ desktop/app.py
@@ -17,7 +17,7 @@ import imageio_ffmpeg
 from PIL import Image, ImageTk
 
 APP_NAME = "TPS Bulk Video Editor"
-APP_VERSION = "1.3.9"
+APP_VERSION = "1.3.10"
 VIDEO_EXTENSIONS = {".mp4", ".mov", ".m4v", ".avi"}
 
 
